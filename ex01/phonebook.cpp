cf37749c64a78b5e58c2bf1928f18ec63b324ae8
therappha <therappha@gmail.com>
2025-04-25 20:04:01 +0100

    made phonebook folder

---
+++ ex01/phonebook.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rafaelfe <rafaelfe@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/25 19:32:19 by rafaelfe          #+#    #+#             */
+/*   Updated: 2025/04/25 20:03:28 by rafaelfe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(int ac, char **av)
+{
+	(void)ac;
+	(void)av;
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello Phonebook!" << std::endl;
+	std::cout << "going home" << std::endl;
+}
