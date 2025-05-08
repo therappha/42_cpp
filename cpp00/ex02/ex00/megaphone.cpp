93fe019cb703fe65ef08385416764284080cdb76
therappha <therappha@gmail.com>
2025-05-08 22:26:27 +0100

    removed .vscode

---
+++ cpp00/ex02/ex00/megaphone.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rafaelfe <rafaelfe@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/25 19:32:19 by rafaelfe          #+#    #+#             */
+/*   Updated: 2025/04/27 15:49:56 by rafaelfe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::toupper;
+using std::cout;
+using std::endl;
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+		return (0);
+	}
+	string str;
+
+	for (int i = 1; i < ac; i++)
+	{
+		str.append(av[i]);
+		if (i + 1 < ac)
+			str.append(" ");
+	}
+	for (size_t i = 0; i < str.length(); i++)
+	{
+		cout << (char)toupper(str[i]);
+	}
+	cout << endl;
+}
---
+++ cpp00/ex02/ex01/Phonebook.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rafaelfe <rafaelfe@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 20:02:19 by rafaelfe          #+#    #+#             */
+/*   Updated: 2025/04/28 23:54:45 by rafaelfe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include "Contact.hpp"
+
+void	print_column(string index, string name, string lastname, string nickname);
+
+class Phonebook
+{
+	private:
+		Contact	contacts[8];
+		int		oldest_contact_id;
+		int		contact_counter;
+
+	public:
+		void		AddContact(Contact contact);
+		bool		ShowAll(void) const;
+		bool		ShowContact(int index) const;
+		int			getCounter() const;
+		Contact		getContact(int index) const;
+		Phonebook();
+		~Phonebook();
+};
+
+#endif
